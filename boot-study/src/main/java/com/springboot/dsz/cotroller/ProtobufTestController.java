785e98c35d07c506fbe83a30856a4ef60d779b14
段守志 <dszazhy1314@163.com>
2021-09-28 11:57:39 +0800

    postman测试protobuf接口通信

--- boot-study/src/main/java/com/springboot/dsz/cotroller/ProtobufTestController.java
+++ boot-study/src/main/java/com/springboot/dsz/cotroller/ProtobufTestController.java
@@ -1,9 +1,7 @@
 package com.springboot.dsz.cotroller;
 
 import com.springboot.dsz.proto_dto.MessageOrder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Auther: ShouZhi@Duan
@@ -16,10 +14,21 @@ public class ProtobufTestController {
     @GetMapping(value = "/test", produces = "application/x-protobuf")
     public MessageOrder.Order getOrder(){
           MessageOrder.Order.Builder order = MessageOrder.Order.newBuilder();
-          order.setName("订单名称");
-          order.setCount("订单数量");
+          order.setName("牙膏");
+          order.setCount("20支");
           MessageOrder.Order build = order.build();
           return build;
+    }
+
+    /**
+     * PostMan测试文件
+     * src/main/postman/order
+     */
+    @PostMapping(value = "test",  produces = "application/x-protobuf", consumes = "application/x-protobuf")
+    public Object postOrder(@RequestBody MessageOrder.Order order){
+        String name = order.getName();
+        String count = order.getCount();
+        return name + " : " + count;
     }
 
 }
